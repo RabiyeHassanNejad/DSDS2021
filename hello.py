28a510d58b6fd1742447d04341035799e17ec365
RabiyeHassanNejad <Rabiye.HassanNejad@Student.HTW-Berlin.de>
2020-10-31 20:40:06 +0100

    Aufgabe 1

---
+++ hello.py
@@ -0,0 +1,12 @@
+# add your import statements at the top...
+
+
+# functions if required below...
+def hello_func(arg):
+    print("Hello " + arg)
+
+
+# the main function (when started from command-line within this if-block...ALWAYS!) last
+if __name__ == "__main__":
+    print("Hello World!")
+    hello_func("function!")
